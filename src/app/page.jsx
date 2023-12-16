07ded6599304ba8b1298eeebd3e2eac1589869f4
sailfuture <78326976+sailfuture@users.noreply.github.com>
2023-12-16 16:01:22 -0500

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,8 +7,6 @@ import { FadeIn, FadeInStagger } from '@/components/FadeIn'
 import { List, ListItem } from '@/components/List'
 import { SectionIntro } from '@/components/SectionIntro'
 import { StylizedImage } from '@/components/StylizedImage'
-import { Testimonial } from '@/components/Testimonial'
-import logoPhobiaDark from '@/images/clients/phobia/logo-dark.svg'
 import imageLaptop from '@/images/laptop.jpg'
 import { loadCaseStudies } from '@/lib/mdx'
 
@@ -140,8 +138,6 @@ export default async function Home() {
           </p>
         </FadeIn>
       </Container>
-
-      <Clients />
 
       <Services />
 
