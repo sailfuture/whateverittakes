287ba6bb116136ce77528ea98c831611b345512f
sailfuture <78326976+sailfuture@users.noreply.github.com>
2023-12-16 18:47:55 -0500

    Changed to Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,214 +1,268 @@
 import { type Metadata } from 'next'
-import Image from 'next/image'
-import Link from 'next/link'
 
+import { Blockquote } from '@/components/Blockquote'
 import { ContactSection } from '@/components/ContactSection'
 import { Container } from '@/components/Container'
-import { FadeIn, FadeInStagger } from '@/components/FadeIn'
+import { FadeIn } from '@/components/FadeIn'
+import { GridList, GridListItem } from '@/components/GridList'
+import { GridPattern } from '@/components/GridPattern'
 import { List, ListItem } from '@/components/List'
+import { PageIntro } from '@/components/PageIntro'
 import { SectionIntro } from '@/components/SectionIntro'
 import { StylizedImage } from '@/components/StylizedImage'
-import { Testimonial } from '@/components/Testimonial'
-import logoBrightPath from '@/images/clients/bright-path/logo-light.svg'
-import logoFamilyFund from '@/images/clients/family-fund/logo-light.svg'
-import logoGreenLife from '@/images/clients/green-life/logo-light.svg'
-import logoHomeWork from '@/images/clients/home-work/logo-light.svg'
-import logoMailSmirk from '@/images/clients/mail-smirk/logo-light.svg'
-import logoNorthAdventures from '@/images/clients/north-adventures/logo-light.svg'
-import logoPhobiaDark from '@/images/clients/phobia/logo-dark.svg'
-import logoPhobiaLight from '@/images/clients/phobia/logo-light.svg'
-import logoUnseal from '@/images/clients/unseal/logo-light.svg'
+import { TagList, TagListItem } from '@/components/TagList'
 import imageLaptop from '@/images/laptop.jpg'
-import { type CaseStudy, type MDXEntry, loadCaseStudies } from '@/lib/mdx'
+import imageMeeting from '@/images/meeting.jpg'
+import imageWhiteboard from '@/images/whiteboard.jpg'
 
-const clients = [
-  ['Phobia', logoPhobiaLight],
-  ['Family Fund', logoFamilyFund],
-  ['Unseal', logoUnseal],
-  ['Mail Smirk', logoMailSmirk],
-  ['Home Work', logoHomeWork],
-  ['Green Life', logoGreenLife],
-  ['Bright Path', logoBrightPath],
-  ['North Adventures', logoNorthAdventures],
-]
-
-function Clients() {
+function Section({
+  title,
+  image,
+  children,
+}: {
+  title: string
+  image: React.ComponentPropsWithoutRef<typeof StylizedImage>
+  children: React.ReactNode
+}) {
   return (
-    <div className="mt-24 rounded-4xl bg-neutral-950 py-20 sm:mt-32 sm:py-32 lg:mt-56">
-      <Container>
-        <FadeIn className="flex items-center gap-x-8">
-          <h2 className="text-center font-display text-sm font-semibold tracking-wider text-white sm:text-left">
-            We’ve worked with hundreds of amazing people
-          </h2>
-          <div className="h-px flex-auto bg-neutral-800" />
-        </FadeIn>
-        <FadeInStagger faster>
-          <ul
-            role="list"
-            className="mt-10 grid grid-cols-2 gap-x-8 gap-y-10 lg:grid-cols-4"
-          >
-            {clients.map(([client, logo]) => (
-              <li key={client}>
-                <FadeIn>
-                  <Image src={logo} alt={client} unoptimized />
-                </FadeIn>
-              </li>
-            ))}
-          </ul>
-        </FadeInStagger>
+    <Container className="group/section [counter-increment:section]">
+      <div className="lg:flex lg:items-center lg:justify-end lg:gap-x-8 lg:group-even/section:justify-start xl:gap-x-20">
+        <div className="flex justify-center">
+          <FadeIn className="w-[33.75rem] flex-none lg:w-[45rem]">
+            <StylizedImage
+              {...image}
+              sizes="(min-width: 1024px) 41rem, 31rem"
+              className="justify-center lg:justify-end lg:group-even/section:justify-start"
+            />
+          </FadeIn>
+        </div>
+        <div className="mt-12 lg:mt-0 lg:w-[37rem] lg:flex-none lg:group-even/section:order-first">
+          <FadeIn>
+            <div
+              className="font-display text-base font-semibold before:text-neutral-300 before:content-['/_'] after:text-neutral-950 after:content-[counter(section,decimal-leading-zero)]"
+              aria-hidden="true"
+            />
+            <h2 className="mt-2 font-display text-3xl font-medium tracking-tight text-neutral-950 sm:text-4xl">
+              {title}
+            </h2>
+            <div className="mt-6">{children}</div>
+          </FadeIn>
+        </div>
+      </div>
+    </Container>
+  )
+}
+
+function Discover() {
+  return (
+    <Section title="Discover" image={{ src: imageWhiteboard }}>
+      <div className="space-y-6 text-base text-neutral-600">
+        <p>
+          We work closely with our clients to understand their{' '}
+          <strong className="font-semibold text-neutral-950">needs</strong> and
+          goals, embedding ourselves in their every day operations to understand
+          what makes their business tick.
+        </p>
+        <p>
+          Our team of private investigators shadow the company director’s for
+          several weeks while our account managers focus on going through their
+          trash. Our senior security experts then perform social engineering
+          hacks to gain access to their{' '}
+          <strong className="font-semibold text-neutral-950">business</strong>{' '}
+          accounts — handing that information over to our forensic accounting
+          team.
+        </p>
+        <p>
+          Once the full audit is complete, we report back with a comprehensive{' '}
+          <strong className="font-semibold text-neutral-950">plan</strong> and,
+          more importantly, a budget.
+        </p>
+      </div>
+
+      <h3 className="mt-12 font-display text-base font-semibold text-neutral-950">
+        Included in this phase
+      </h3>
+      <TagList className="mt-4">
+        <TagListItem>In-depth questionnaires</TagListItem>
+        <TagListItem>Feasibility studies</TagListItem>
+        <TagListItem>Blood samples</TagListItem>
+        <TagListItem>Employee surveys</TagListItem>
+        <TagListItem>Proofs-of-concept</TagListItem>
+        <TagListItem>Forensic audit</TagListItem>
+      </TagList>
+    </Section>
+  )
+}
+
+function Build() {
+  return (
+    <Section title="Build" image={{ src: imageLaptop, shape: 1 }}>
+      <div className="space-y-6 text-base text-neutral-600">
+        <p>
+          Based off of the discovery phase, we develop a comprehensive roadmap
+          for each product and start working towards delivery. The roadmap is an
+          intricately tangled mess of technical nonsense designed to drag the
+          project out as long as possible.
+        </p>
+        <p>
+          Each client is assigned a key account manager to keep lines of
+          communication open and obscure the actual progress of the project.
+          They act as a buffer between the client’s incessant nagging and the
+          development team who are hard at work scouring open source projects
+          for code to re-purpose.
+        </p>
+        <p>
+          Our account managers are trained to only reply to client emails after
+          9pm, several days after the initial email. This reinforces the general
+          aura that we are very busy and dissuades clients from asking for
+          changes.
+        </p>
+      </div>
+
+      <Blockquote
+        author={{ name: 'Debra Fiscal', role: 'CEO of Unseal' }}
+        className="mt-12"
+      >
+        Studio were so regular with their progress updates we almost began to
+        think they were automated!
+      </Blockquote>
+    </Section>
+  )
+}
+
+function Deliver() {
+  return (
+    <Section title="Deliver" image={{ src: imageMeeting, shape: 2 }}>
+      <div className="space-y-6 text-base text-neutral-600">
+        <p>
+          About halfway through the Build phase, we push each project out by 6
+          weeks due to a change in{' '}
+          <strong className="font-semibold text-neutral-950">
+            requirements
+          </strong>
+          . This allows us to increase the budget a final time before launch.
+        </p>
+        <p>
+          Despite largely using pre-built components, most of the{' '}
+          <strong className="font-semibold text-neutral-950">progress</strong>{' '}
+          on each project takes place in the final 24 hours. The development
+          time allocated to each client is actually spent making augmented
+          reality demos that go viral on Twitter.
+        </p>
+        <p>
+          We ensure that the main pages of the site are{' '}
+          <strong className="font-semibold text-neutral-950">
+            fully functional
+          </strong>{' '}
+          at launch — the auxiliary pages will, of course, be lorem ipusm shells
+          which get updated as part of our exorbitant{' '}
+          <strong className="font-semibold text-neutral-950">
+            maintenance
+          </strong>{' '}
+          retainer.
+        </p>
+      </div>
+
+      <h3 className="mt-12 font-display text-base font-semibold text-neutral-950">
+        Included in this phase
+      </h3>
+      <List className="mt-8">
+        <ListItem title="Testing">
+          Our projects always have 100% test coverage, which would be impressive
+          if our tests weren’t as porous as a sieve.
+        </ListItem>
+        <ListItem title="Infrastructure">
+          To ensure reliability we only use the best Digital Ocean droplets that
+          $4 a month can buy.
+        </ListItem>
+        <ListItem title="Support">
+          Because we hold the API keys for every critical service your business
+          uses, you can expect a lifetime of support, and invoices, from us.
+        </ListItem>
+      </List>
+    </Section>
+  )
+}
+
+function Values() {
+  return (
+    <div className="relative mt-24 pt-24 sm:mt-32 sm:pt-32 lg:mt-40 lg:pt-40">
+      <div className="absolute inset-x-0 top-0 -z-10 h-[884px] overflow-hidden rounded-t-4xl bg-gradient-to-b from-neutral-50">
+        <GridPattern
+          className="absolute inset-0 h-full w-full fill-neutral-100 stroke-neutral-950/5 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)]"
+          yOffset={-270}
+        />
+      </div>
+
+      <SectionIntro
+        eyebrow="Our values"
+        title="Balancing reliability and innovation"
+      >
+        <p>
+          We strive to stay at the forefront of emerging trends and
+          technologies, while completely ignoring them and forking that old
+          Rails project we feel comfortable using. We stand by our core values
+          to justify that decision.
+        </p>
+      </SectionIntro>
+
+      <Container className="mt-24">
+        <GridList>
+          <GridListItem title="Meticulous">
+            The first part of any partnership is getting our designer to put
+            your logo in our template. The second step is getting them to do the
+            colors.
+          </GridListItem>
+          <GridListItem title="Efficient">
+            We pride ourselves on never missing a deadline which is easy because
+            most of the work was done years ago.
+          </GridListItem>
+          <GridListItem title="Adaptable">
+            Every business has unique needs and our greatest challenge is
+            shoe-horning those needs into something we already built.
+          </GridListItem>
+          <GridListItem title="Honest">
+            We are transparent about all of our processes, banking on the simple
+            fact our clients never actually read anything.
+          </GridListItem>
+          <GridListItem title="Loyal">
+            We foster long-term relationships with our clients that go beyond
+            just delivering a product, allowing us to invoice them for decades.
+          </GridListItem>
+          <GridListItem title="Innovative">
+            The technological landscape is always evolving and so are we. We are
+            constantly on the lookout for new open source projects to clone.
+          </GridListItem>
+        </GridList>
       </Container>
     </div>
   )
 }
 
-function CaseStudies({
-  caseStudies,
-}: {
-  caseStudies: Array<MDXEntry<CaseStudy>>
-}) {
-  return (
-    <>
-      <SectionIntro
-        title="Harnessing technology for a brighter future"
-        className="mt-24 sm:mt-32 lg:mt-40"
-      >
-        <p>
-          We believe technology is the answer to the world’s greatest
-          challenges. It’s also the cause, so we find ourselves in bit of a
-          catch 22 situation.
-        </p>
-      </SectionIntro>
-      <Container className="mt-16">
-        <FadeInStagger className="grid grid-cols-1 gap-8 lg:grid-cols-3">
-          {caseStudies.map((caseStudy) => (
-            <FadeIn key={caseStudy.href} className="flex">
-              <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
-                <h3>
-                  <Link href={caseStudy.href}>
-                    <span className="absolute inset-0 rounded-3xl" />
-                    <Image
-                      src={caseStudy.logo}
-                      alt={caseStudy.client}
-                      className="h-16 w-16"
-                      unoptimized
-                    />
-                  </Link>
-                </h3>
-                <p className="mt-6 flex gap-x-2 text-sm text-neutral-950">
-                  <time
-                    dateTime={caseStudy.date.split('-')[0]}
-                    className="font-semibold"
-                  >
-                    {caseStudy.date.split('-')[0]}
-                  </time>
-                  <span className="text-neutral-300" aria-hidden="true">
-                    /
-                  </span>
-                  <span>Case study</span>
-                </p>
-                <p className="mt-6 font-display text-2xl font-semibold text-neutral-950">
-                  {caseStudy.title}
-                </p>
-                <p className="mt-4 text-base text-neutral-600">
-                  {caseStudy.description}
-                </p>
-              </article>
-            </FadeIn>
-          ))}
-        </FadeInStagger>
-      </Container>
-    </>
-  )
-}
-
-function Services() {
-  return (
-    <>
-      <SectionIntro
-        eyebrow="Services"
-        title="We help you identify, explore and respond to new opportunities."
-        className="mt-24 sm:mt-32 lg:mt-40"
-      >
-        <p>
-          As long as those opportunities involve giving us money to re-purpose
-          old projects — we can come up with an endless number of those.
-        </p>
-      </SectionIntro>
-      <Container className="mt-16">
-        <div className="lg:flex lg:items-center lg:justify-end">
-          <div className="flex justify-center lg:w-1/2 lg:justify-end lg:pr-12">
-            <FadeIn className="w-[33.75rem] flex-none lg:w-[45rem]">
-              <StylizedImage
-                src={imageLaptop}
-                sizes="(min-width: 1024px) 41rem, 31rem"
-                className="justify-center lg:justify-end"
-              />
-            </FadeIn>
-          </div>
-          <List className="mt-16 lg:mt-0 lg:w-1/2 lg:min-w-[33rem] lg:pl-4">
-            <ListItem title="Web development">
-              We specialise in crafting beautiful, high quality marketing pages.
-              The rest of the website will be a shell that uses lorem ipsum
-              everywhere.
-            </ListItem>
-            <ListItem title="Application development">
-              We have a team of skilled developers who are experts in the latest
-              app frameworks, like Angular 1 and Google Web Toolkit.
-            </ListItem>
-            <ListItem title="E-commerce">
-              We are at the forefront of modern e-commerce development. Which
-              mainly means adding your logo to the Shopify store template we’ve
-              used for the past six years.
-            </ListItem>
-            <ListItem title="Custom content management">
-              At Studio we understand the importance of having a robust and
-              customised CMS. That’s why we run all of our client projects out
-              of a single, enormous Joomla instance.
-            </ListItem>
-          </List>
-        </div>
-      </Container>
-    </>
-  )
-}
-
 export const metadata: Metadata = {
+  title: 'Our Process',
   description:
-    'We are a development studio working at the intersection of design and technology.',
+    'We believe in efficiency and maximizing our resources to provide the best value to our clients.',
 }
 
-export default async function Home() {
-  let caseStudies = (await loadCaseStudies()).slice(0, 3)
-
+export default function Process() {
   return (
     <>
-      <Container className="mt-24 sm:mt-32 md:mt-56">
-        <FadeIn className="max-w-3xl">
-          <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Award-winning development studio based in Denmark.
-          </h1>
-          <p className="mt-6 text-xl text-neutral-600">
-            We are a development studio working at the intersection of design
-            and technology. It’s a really busy intersection though — a lot of
-            our staff have been involved in hit and runs.
-          </p>
-        </FadeIn>
-      </Container>
+      <PageIntro eyebrow="Our process" title="How we work">
+        <p>
+          We believe in efficiency and maximizing our resources to provide the
+          best value to our clients. The primary way we do that is by re-using
+          the same five projects we’ve been developing for the past decade.
+        </p>
+      </PageIntro>
 
-      <Clients />
+      <div className="mt-24 space-y-24 [counter-reset:section] sm:mt-32 sm:space-y-32 lg:mt-40 lg:space-y-40">
+        <Discover />
+        <Build />
+        <Deliver />
+      </div>
 
-      <CaseStudies caseStudies={caseStudies} />
-
-      <Testimonial
-        className="mt-24 sm:mt-32 lg:mt-40"
-        client={{ name: 'Phobia', logo: logoPhobiaDark }}
-      >
-        The team at Studio went above and beyond with our onboarding, even
-        finding a way to access the user’s microphone without triggering one of
-        those annoying permission dialogs.
-      </Testimonial>
-
-      <Services />
+      <Values />
 
       <ContactSection />
     </>
