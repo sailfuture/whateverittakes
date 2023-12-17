23d533c7d6b10e3c45f3b5108545b8cceea0b3dd
sailfuture <78326976+sailfuture@users.noreply.github.com>
2023-12-16 22:47:25 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link'
 
 import { Container } from '@/components/Container'
 import { FadeIn } from '@/components/FadeIn'
-import { Logo } from '@/components/Logo'
 import { socialMediaProfiles } from '@/components/SocialMedia'
 
 const navigation = [
